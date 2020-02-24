7d0ccdd6d42b74f3685cf0edb08a9b4559c6ebaf
Pratik <pr.tikgohil@gmail.com>
2020-02-24 19:21:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function breakOut(array, changeValue, stopValue) {
   for (let elem in array) {
     elem = changeValue
-    if (elem == stopValue) {
-      break
-    }
+    
     return array
   }
 }

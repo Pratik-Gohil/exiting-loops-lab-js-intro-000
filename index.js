7ad314fce6a3e5057657fe95980cc50507b6f0c5
Pratik <pr.tikgohil@gmail.com>
2020-02-24 19:25:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function breakOut(array, changeValue, stopValue) {
-  for (let i = 0; i < array.length; i++) {
+  while (true) {
     array[i] = changeValue
     if (array[i] == stopValue) {
       break
